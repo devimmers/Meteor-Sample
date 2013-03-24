1b32e1a321ca94b525eee1243e6d82d6f4c7d644
Yaroslav <wenqeer@gmail.com>
2013-03-25 01:14:37 +0200

    Templates refactoring

--- Chat.js
+++ Chat.js
@@ -4,101 +4,72 @@ if(Meteor.isClient) {
 
   var subscr = Meteor.subscribe("messages");
 
-  ////////// Helpers for in-place editing //////////
-  // Returns an event_map key for attaching "ok/cancel" events to
-  // a text input (given by selector)
-  var okcancel_events = function (selector) {
-    return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
-  };
+  _(Template).extend({
 
-  // Creates an event handler for interpreting "escape", "return", and "blur"
-  // on a text field and calling "ok" or "cancel" callbacks.
-  var make_okcancel_handler = function (options) {
-    var ok = options.ok || function () {};
-    var cancel = options.cancel || function () {};
+    // Entry View
+    entry: {
 
-    return function (evt) {
-      if (evt.type === "keydown" && evt.which === 27) {
-        // escape = cancel
-        cancel.call(this, evt);
-      } else if (evt.type === "keyup" && evt.which === 13) {
-        // blur/return/enter = ok/submit if non-empty
-        var value = String(evt.target.value || "");
-        if (value)
-          ok.call(this, value, evt);
-        else
-          cancel.call(this, evt);
+      events: {
+        //Send message by enter or btn click
+        'click #send-message, keyup, keydown #messageBox': function(e) {
+          if (e.type === "keyup" || e.type === "keydown" && e.which !== 13)
+            return;
+
+          var ts  = new Date().toLocaleTimeString(),
+              msg = $('#messageBox');
+          
+          if (msg.val() !== '') {        
+            Meteor.call('add_msg', msg.val());
+          }
+
+          msg.val('');
+        },
+        //Clear msg btn
+        'click #clear-messages': function() {
+          if (confirm('Are you sure you want to remove all todo items from the current list? This action cannot be undone.')) {
+             console.log("clear");
+          }
+        }
       }
-    };
-  };
+    },
 
-   Template.chat.signed_in = function () {
-      if(Meteor.userId() != null) {
-        return false; 
-      } 
-      return true;
-   };
+    //Chat View
 
+    chat: {
 
-  //Send message function
-  function sendMessage(text) {
-      var ts = new Date().toLocaleTimeString();    
-      
-      if (text !== '') {        
-        Meteor.call('add_msg', text);
+      signed_in: function () {
+        if(Meteor.userId() != null) {
+          return false; 
+        } 
+        return true;
+      },
+
+      events: {
+        'click #logout-btn': function(event) {
+          Session.set('user_id', null);
+          Session.set('verified', false);
+          Template.register.signed_in;
+        }
       }
+    },
 
-      $('#messageBox').val('');
-      event.preventDefault();
-      event.stopPropagation();
-  }
+    //User list View  
+    users_names: {
 
-  //Events Area
-  Template.entry.events = {};
+      events: {
+      //Whisper Action
+        'click #users a': function(event) {
+          event.preventDefault();
+          var message = document.getElementById('messageBox'),
+              name    = event.currentTarget.getAttribute("href");
 
-  //Enter Send Action
-  Template.entry.events[okcancel_events('#messageBox')] = make_okcancel_handler({
-    ok: function(text, event ) {
-        sendMessage(text);
-        event.target.value = "";
-    }
-  });
-
-  //Send Action
-  Template.entry.events['click #send-message'] = function() {
-      var message = document.getElementById('messageBox');
-      sendMessage(message.value);
-  };
-
-  //Send Action
-  Template.entry.events['click #clear-messages'] = function() {
-    if (confirm('Are you sure you want to remove all todo items from the current list? This action cannot be undone.')) {
-       console.log("clear");
-       
-    }
-  };
-
-
-  Template.chat.events = {
-    'click #logout-btn': function(event) {
-      Session.set('user_id', null);
-      Session.set('verified', false);
-      Template.register.signed_in;
-    }
-  };
-
-  //Whisper Action
-  Template.users_names.events = {
-    'click #users a': function(event) {
-      event.preventDefault();
-      var message = document.getElementById('messageBox'),
-          name    = event.currentTarget.getAttribute("href");
-      message.value = name + ': ';
-      message.focus();
-      message.setSelectionRange(name.length+2,name.length+2);
-    }
-  };
-
+          message.value = name + ': ';
+          message.focus();
+          message.setSelectionRange(name.length+2,name.length+2);
+        }
+      }
+    },
+  
   //Users name list loaging
 /*  Template.users_names.users = function () {
     var users = Users.find({name: { $exists: true }}, { sort: {name: 1} });
@@ -106,9 +77,13 @@ if(Meteor.isClient) {
   };
 */
   //Show all messages
-  Template.messages.messages = function () {
-    return Messages.find({}, {sort:{time:-1}});
-  }
+    messages: messages = function () {
+      return Messages.find({}, {sort:{time:-1}});
+    }
+
+  });
+
+
 
   //Checking for leaving users
   Meteor.setInterval(function () {
@@ -135,6 +110,7 @@ if (Meteor.is_server) {
 
    Meteor.startup(function () {
         disableClientMongo();
+        //Messages.remove({});
    });
 
   //Publish Messages and Users list 
